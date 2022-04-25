045adbce92d8da9de0f942c1601fc570d08ffeb1
Luis Lucero <clucero3696@gmail.com>
2022-04-25 15:25:56 -0700

    Added mqs to Heading, Removed Articles

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -16,14 +16,14 @@ import {
 const Heading = props => {
 
     return (
-        <Box p='2rem 1rem' {...props}>
+        <Box p='2rem 1rem' w={['90%', '90%', '65%', '65%', '65%']} m='0 auto' {...props}>
             <HStack>
                 <Icon w={6} h={6} as={Gi3DStairs} _hover={{ color: 'green.700' }}/>
                 <Link variant='primary' to='/' as={routerLink}>
                     <Text>llucero</Text>
                 </Link>
                 <Spacer />
-                <Link variant='primary' to='/articles' as={routerLink}>Articles</Link>
+                {/* <Link variant='primary' to='/articles' as={routerLink}>Articles</Link> */}
                 <Link variant='primary' to='/projects' as={routerLink}>Projects</Link>
             </HStack>
         </Box>
