08f1ece1e946b48a17698a7ae59207a4e348c3ba
Luis Lucero <clucero3696@gmail.com>
2022-04-29 18:43:00 -0700

    Fixed Name & Punctuation

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -15,14 +15,14 @@ const Welcome = () => {
   return (
     <Box padding='4rem' >
       <VStack spacing='5rem'>
-        <Heading>Hey, I'm Carlos</Heading>
+        <Heading>Hey, I'm Luis</Heading>
         <Box w='20rem'>
           <VStack spacing={5}>
             <Text>
               I am a web developer and I've recently graduated with a Bachelors Degree in Computer Science.
               Currently, I am learning web technologies like JavaScript, React, & Node. I have built a handful of applications
               to demonstrate my capabilites and I am looking to join a team where I can continue to develop my skills and contribute to 
-              larger projects
+              larger projects.
             </Text>
             <Text>
               In my free time I enjoy going to live music events, finding new places for food & drink, playing disc golf, and spending time with my family and friends.
