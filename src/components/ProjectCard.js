27df62f9726268899fcd83406df9517434219dc4
Luis Lucero <clucero3696@gmail.com>
2022-08-18 20:04:21 -0700

    Updated Projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,9 +5,9 @@ import {
   Text,
   Image,
   Heading,
-  AspectRatio, 
   Link,
   HStack,
+  Flex,
 } from '@chakra-ui/react'
 
 
@@ -15,22 +15,22 @@ const ProjectCard = (props) => {
 
 
   return (
-    <Box maxW='sm' minW='sm' display='flex' flexDir='column' alignItems='center' m='5rem auto' boxShadow='2xl' borderRadius='md'>
-      <AspectRatio maxW='20rem' minW='20rem' ratio={4/3} >
-        <Image src={props.src} boxShadow='2xl' pt='1rem'/>
-      </AspectRatio>
-      <Box m='2rem' borderTop='1px solid' borderColor='purple.800'>
-        <Heading size='md'>{props.name}</Heading>
-        <Text>{props.description}</Text>
-      </Box>
-      <HStack spacing={25}>
-        <Link variant='primary' href={props.link}>
-          <Text>Live</Text>
-        </Link>
-        <Link variant='primary' href={props.git}>
-          <Text>Git</Text>
-        </Link>
-      </HStack>
+    <Box w='sm' m='5rem auto' boxShadow='2xl' borderRadius='md'>
+      <Flex flexDir='column' alignItems='center' gap='1rem'>
+        <Image src={props.src} pt='1rem' maxW='20rem' maxH='20rem'/>
+        <Box w='90%' m='0 auto' borderTop='1px solid' borderColor='purple.800'>
+          <Heading size='md'>{props.name}</Heading>
+          <Text>{props.description}</Text>
+        </Box>
+        <HStack spacing={25}>
+          <Link variant='primary' href={props.link}>
+            <Text>Live</Text>
+          </Link>
+          <Link variant='primary' href={props.git}>
+            <Text>Git</Text>
+          </Link>
+        </HStack>
+      </Flex>
     </Box>
   )
 }
--- src/data.js
+++ src/data.js
@@ -7,6 +7,13 @@ const projects = [
     link: 'https://jetboats.netlify.app/',
     git: 'https://github.com/clucero36/jetboats.git',
   },
+  { 
+    name: 'Spotify Clone',
+    description: 'Spotify UI made with React JS & Spotify OAuth 2.0. Work in Progress. Email me for live access',
+    src: 'spot1stPage.png',
+    link: '',
+    git: 'https://github.com/clucero36/spotify-clone.git',
+  },
   {
     name: 'Blogger Landing Page',
     description: 'Landing page developed with HTML, CSS, & JavsScript',
@@ -15,19 +22,12 @@ const projects = [
     git: 'https://github.com/clucero36/Blogr.git',
   },
   {
-    name: 'Widgets',
-    description: 'Widgets application developed with JS, React, & Chakra UI component library',
-    src: 'widgetsNew.png',
-    link: 'https://five-widgets.netlify.app/',
-    git: 'https://github.com/clucero36/widgets.git'
+    name: 'Tic-Tac-Toe',
+    description: 'Classic Tic-Tac-Toe game developed with React & Chakra UI',
+    src: 'tttNew.png',
+    link: 'https://threepeat.netlify.app/',
+    git: 'https://github.com/clucero36/TicTacToe.git'
   },
-  { 
-    name: 'Spotify Clone',
-    description: 'Spotify UI made with React JS & Spotify OAuth 2.0. Work in Progress. Email me for live access',
-    src: 'spot1stPage.png',
-    link: '',
-    git: 'https://github.com/clucero36/spotify-clone.git',
-  }
 ]
 
 export function getProjects() {
