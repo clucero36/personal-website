3171dd05d0bbbf42dc6007def74a967a7b1e71d2
llucero <clucero3696@gmail.com>
2022-12-16 17:41:49 -0800

    Proj Update, CardFix

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import {
   Box, 
   Text,
   Image,
-  Heading,
   Link,
   HStack,
   Flex,
@@ -18,8 +16,8 @@ const ProjectCard = (props) => {
     <Box w='sm' h='25rem' minW='350px' boxShadow='2xl' borderRadius='md'>
       <Flex flexDir='column' alignItems='center' gap='.75rem' justifyContent='space-between' h='100%'>
         <Image src={props.src} w='95%' h='15rem' pt='1rem'/>
-        <Box w='95%' borderTop='1px solid' borderColor='purple.800'>
-          <Heading size='md' pb='.5rem'>{props.name}</Heading>
+        <Box w='95%' borderTop='1px solid' borderColor='purple.800' h='5rem'>
+          <Text fontSize='lg' fontWeight='bold' isTruncated pb='.5rem'>{props.name}</Text>
           <Text>{props.description}</Text>
         </Box>
         <HStack spacing={25}>
--- src/data.js
+++ src/data.js
@@ -2,35 +2,38 @@
 const projects = [
   {
     name: 'Jet Boat Classifieds',
-    description: 'E-Commerce application developed with react, firebase, & stripe payment processing.',
+    description: 'E-Commerce business application. React, Firebase, & Stripe payment processing.',
     src: 'shopScreen.png',
     link: 'https://jetboats.netlify.app/',
     git: 'https://github.com/clucero36/jetboats.git',
   },
   { 
     name: 'Spotify Clone',
-    description: 'Spotify UI made with React JS & Spotify OAuth 2.0. Work in Progress. Email me for live access.',
+    description: 'Spotify Web Playback App. React, Spotify OAuth 2.0, & Firebase. Email me for live access.',
     src: 'spot1stPage.png',
     link: '',
     git: 'https://github.com/clucero36/spotify-clone.git',
   },
   {
     name: 'Blogger Landing Page',
-    description: 'Landing page developed with HTML, CSS, & JavsScript',
+    description: 'Business landing page. HTML, CSS, & JavsScript.',
     src: 'bloggrNew.png',
     link: 'https://blogrbusiness.netlify.app/',
     git: 'https://github.com/clucero36/Blogr.git',
   },
   {
     name: 'Tic-Tac-Toe',
-    description: 'Classic Tic-Tac-Toe game developed with React & Chakra UI',
+    description: 'Classic Tic-Tac-Toe. JavaScript, React, & Chakra UI',
     src: 'tttNew.png',
     link: 'https://threepeat.netlify.app/',
     git: 'https://github.com/clucero36/TicTacToe.git'
   },
   {
-    name: 'Coming Soon: Appointments',
-    description: 'Appointment scheduler being developed with Next.js & Firebase.',
+    name: 'Salon WebApp',
+    description: 'Business Web App. Bookings Coming Soon. Next.js, Node/Express, ChakraUI.',
+    src: 'salon-webapp.PNG',
+    link: 'https://salon-webapp.vercel.app',
+    git: 'https://github.com/clucero36/salon-webapp.git'
   }
 ]
 
