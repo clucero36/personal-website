41341ef3febe2ae38547cbdc0f5f05434965f998
llucero <clucero3696@gmail.com>
2023-07-28 13:31:26 -0700

    bg color

--- src/themes/globalStyles.js
+++ src/themes/globalStyles.js
@@ -3,8 +3,8 @@ export const GlobalStyles = {
   global: (props) => ({
     body: {
       fontFamily: "Roboto",
-      color: props.colorMode === 'dark' ?  'gray.600' : 'white',
-      backgroundColor: props.colorMode === 'dark' ? 'blue.300' : 'gray.800', 
+      color: props.colorMode === 'dark' ?  'white' : 'white',
+      backgroundColor: props.colorMode === 'dark' ? 'gray.700' : 'gray.800', 
       margin: '0 auto',
     }
   }),
