bb190ecb48162d7d7e3190251a5aa73cab7ae5d2
llucero <clucero3696@gmail.com>
2023-08-04 14:47:48 -0700

    summary update

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
             <Text>
               I am a Computer Science graduate and a Web Programmer at Trinet Internet Solutions in Irvine, California. 
               I have built applications with JavaScript tech stacks and I'm currently building web applications with content managements systems.
-              I'm always looking for ways to improve as a developer and I hope to one day leverage my skill set to start a business of my own. 
+              I'm always looking for ways to improve as a developer, and I hope to join a team of like minded individuals dedicated to producing quality software products.
             </Text>
             <Text>
-              In my free time I enjoy going to live music events, finding new places for food & drink, playing disc golf, and spending time with my family and friends.
+              In my free time I enjoy going to live music events, finding new places for food & drink, playing disc golf, gaming, making generative art, and spending time with my family and friends.
             </Text>
           </VStack>
         </Box>
